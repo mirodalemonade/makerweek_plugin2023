a126031ddcdd6b05bc001ad9367b7f20a31a4b9c
YasushiIshi <lemonade.swag.boy@gmail.com>
2023-08-10 15:35:22 +0900

    frame colour updated

--- code.ts
+++ code.ts
@@ -16,17 +16,18 @@ if (figma.editorType === "figma") {
 
     // Default characters and color
     let characters = ['M', 'A', 'K', 'E', 'R', 'W', 'E', 'E', 'K', '😊'];
-    let fillColor = { r: 0.2, g: 0.5, b: 0.1 }; // Default green color
+    let fillColor = { r: 0.2, g: 0.5, b: 0.1 }; 
 
+    // 5th and 10th
     if (makerweekFrames.length === 5) {
       characters = ['✋', 'A', 'M', 'A', 'Z', 'I', 'N', 'G', '😍'];
-      fillColor = { r: 0.0, g: 0.8, b: 0.8 }; // Light blue
+      fillColor = { r: 0.0, g: 0.8, b: 0.8 }; 
     } else if (makerweekFrames.length === 10) {
-      characters = ['S', 'U', 'P', 'E', 'R', '😊'];
-      fillColor = { r: 0.0, g: 0.8, b: 0.8 }; // Light blue
+      characters = ['💛', 'S', 'U', 'P', 'E', 'R', '😊'];
+      fillColor = { r: 0.0, g: 0.8, b: 0.8 }; 
     }
 
-    // Set the frame's fill color
+    // Default frame's fill color
     frame.fills = [{ type: 'SOLID', color: fillColor }];
 
     // Find a text node with the name "counter" to track plugin executions
@@ -44,17 +45,6 @@ if (figma.editorType === "figma") {
         counterNode.characters = (parseInt(counterNode.characters) + 1).toString();
     }
 
-
-
-    // // Choose characters based on the number of "makerweek" frames
-    // let characters = ['M', 'A', 'K', 'E', 'R', 'W', 'E', 'E', 'K', '😊']; // Default
-    // if (makerweekFrames.length === 5) {
-    //   　characters = ['✋', 'A', 'M', 'A', 'Z', 'I', 'N', 'G', '😍'];
-    // } 
-    // else if (makerweekFrames.length === 10) {
-    //   　characters = ['💛', 'S', 'U', 'P', 'E', 'R', '😊'];
-    // }
-
     // Rainbow colors
     const rainbowColors = [
         { r: 1, g: 0, b: 0 }, 
@@ -66,7 +56,7 @@ if (figma.editorType === "figma") {
         { r: 0.5, g: 0, b: 0.5 }, 
     ];
 
-    // Random color
+    // Randomize text colors
       const getRandomColor = () => {
         const randomIndex = Math.floor(Math.random() * rainbowColors.length);
         return rainbowColors[randomIndex];
